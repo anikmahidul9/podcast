95b0bf5fff497dcf85cea03edbb7b8405d8e899f
anikmahidul9 <mahidulanik9@gmail.com>
2024-06-28 16:34:10 +0600

    homepage_1

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -6,11 +8,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
-        <LeftSidebar/>
-        {children}
-        <p>Right side of layout</p>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
+        <LeftSidebar />
+        <section className="border-2 min-h-screen flex-1 flex-col px-4 sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
+              <Image src="/icons/logo.svg" width={30} height={30} alt="menu-item"/>
+              MobileNav
+            </div>
+            <div className="flex flex-col md:pb-14">
+              Toaster
+              {children}
+            </div>
+          </div>
+        </section>
+       <RightSidebar/>
       </main>
     </div>
   );
